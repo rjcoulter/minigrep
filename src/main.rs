74071e054cd3866111ec04cf11d62ef8642d6311
Ryan Coulter <rjc8ez@virginia.edu>
2019-01-08 16:17:23 -0500

    Read in inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Read in arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Save values read in
+    let query = &args[1];
+    let filename = &args[2];
 }
