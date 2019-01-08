1936d11d278050c39d6adc40c2dc9a6861a4f471
Ryan Coulter <rjc8ez@virginia.edu>
2019-01-08 17:08:25 -0500

    Add search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,43 @@
 use std::error::Error;
 use std::fs;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+       if line.contains(query) {    
+           result.push(line);
+       } 
+    }
+    result
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+    
     Ok(())
 }
 
