07d149a3d274c10c2dd825fe40532c75ffcf2100
Ryan Coulter <rjc8ez@virginia.edu>
2019-01-08 16:38:03 -0500

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
-    // Read in arguments
     let args: Vec<String> = env::args().collect();
 
-    // Save values read in
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing the arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Error reading the file");
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+struct Config {
+    query: String,
+    filename: String,
 }
